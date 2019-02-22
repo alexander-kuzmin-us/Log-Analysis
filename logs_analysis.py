ea385f7deba856b7d1f91e8cb3f87e517792a6cc
Aleksandr Kuzmin <alex.kuzminn@gmail.com>
2019-02-21 23:03:24 -0500

    logs_analysis.py was tested on http://pep8online

--- logs_analysis.py
+++ logs_analysis.py
@@ -22,52 +22,55 @@ def run_query(query):
 
 
 # 1. Most popular articles
-def get_top_three_articles():
 # Query 1 (I used the JOIN clause to select titles from 2 tables log and
 # articles ON slug and path. And Substr 10 char lengh str from path in log to
 # find a matched  slug in articles)
+def get_top_three_articles():
+# Query String 1
     query = """
-    SELECT title, COUNT(*) AS num
+    SELECT title, COUNT(*) as num
     FROM articles
     JOIN log
     ON articles.slug LIKE substr(log.path, 10)
     GROUP BY title
-    ORDER BY num
-    DESC
+    ORDER BY num DESC
     LIMIT 3;
     """
 
 # Run Query 1
-    result = run_query(query)
-# Print result Query 1
-print ('TOP 3 MOST POPULAR ARTICLES OF ALL TIME:\n')
-for result in results:
-    print('"{title}" - {num} total views').
-    format(title=result[0], num=result[1]))
+    results = run_query(query)
+
+# Print Results 1
+    print('TOP THREE MOST POPULAR ARTICLES OF ALL TIME:\n')
+    for result in results:
+        print('"{title}" - {num} total views'.
+        format(title=result[0], num=result[1]))
+
 
 # 2. Most popular authors
 def get_top_article_authors():
-# Query 2
+
+# Query 2 String
     query = """
     SELECT authors.name, COUNT(*) AS num
     FROM authors
     JOIN articles
-    ON autors.id = articles.autor
+    ON authors.id = articles.author
     JOIN log
     ON articles.slug LIKE substr(log.path, 10)
-    GROUP BY autors.name,
-    ORDER BY num
-    DESC
+    GROUP BY authors.name
+    ORDER BY num DESC
     LIMIT 3;
     """
-# Run Query 2
-    result = run_query(query)
 
-# Print result 2
-    print('TOP 3 MOST POPULAR AUTHORS:\n')
+# Run Query 2
+    results = run_query(query)
+
+# Print Results 2
+    print('TOP THREE MOST POPULAR AUTHORS OF ALL TIME:\n')
     for result in results:
-        print('"{author}" - {num} total views').
-        format(author=result[0], num=result[1]))
+        print('"{title}" - {num} total views'.
+        format(title=result[0], num=result[1]))
 
 
 # 3. Days which more then 1% of HTTP requests errors
