39a3f6b19afc7ddbc64ed7bd1bbd4abd7e94c76f
Aleksandr Kuzmin <alex.kuzminn@gmail.com>
2019-02-21 21:50:41 -0500

    Print result for Query 1 added

--- logs_analysis.py
+++ logs_analysis.py
@@ -23,7 +23,9 @@ def run_query(query):
 
 # 1. Most popular articles
 def get_top_three_articles():
-# Query 1
+# Query 1 (I used the JOIN clause to select titles from 2 tables log and
+# articles ON slug and path. And Substr 10 char lengh str from path in log to
+# find a matched  slug in articles)
     query = """
     SELECT title COUNT(*) AS num
     FROM articles
@@ -37,3 +39,8 @@ def get_top_three_articles():
 
 # Run Query 1
     result = run_query(query)
+# Print result Query 1
+print ('TOP 3 MOST POPULAR ARTICLES OF ALL TIME\n')
+for result in results:
+    print('"{title}" - {num} total views').
+    format(title=result[0], num=result[1]))
